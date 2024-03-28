9c170813d6dae145d3107016da1195e8715ce1e3
dheeru2504 <dk.dheeraj2504@gmail.com>
2024-03-28 21:35:56 +0530

    updated an project

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -5,58 +5,54 @@ import IMG4 from "../../assets/IMG4.png";
 import IMG5 from "../../assets/IMG5.png";
 import IMG7 from "../../assets/IMG7.png";
 import IMG8 from "../../assets/IMG8.png";
+import IMG9 from "../../assets/IMG9.png";
 
 
 const data = [
-  // {
-  //   id :1,
-  //   image:IMG1,
-  //   title:'Drum Kit',
-  //   github:'https://github.com/Dheeru-k/Drum-Kit',
-  //   demo:'https://dheeru-k.github.io/Drum-Kit/'
-  // },
-  // {
-  //   id :2,
-  //    image:IMG2,
-  //   title:'Dice Game',
-  //   github:'https://github.com/Dheeru-k/Dice-Game',
-  //   demo:'https://dheeru-k.github.io/Dice-Game/'
-  // },
+
   {
     id: 1,
-    image: IMG3,
-    title: "Keep Notes",
-    github: "https://github.com/Dheeru-k/Kepper-App",
-    demo: "https://keepnotees.netlify.app/",
-  },
-  {
-    id: 2,
-    image: IMG4,
-    title: "To Do List",
-    github: "https://github.com/Dheeru-k/todoList",
-    demo: "",
-  },
-  {
-    id: 3,
-    image: IMG5,
-    title: "Share Your Secrets",
-    github: "https://github.com/Dheeru-k/Secrets-1",
-    demo: "",
-  },
-  {
-    id: 4,
     image: IMG7,
     title: "E Commerce ",
     github: "https://github.com/Dheeru-k/E-commerce",
     demo: "https://front-dv26.onrender.com",
   },
   {
-    id: 5,
+    id: 2,
+    image: IMG9,
+    title: "Chat App",
+    github: "https://github.com/dheeru2504/Chat_App",
+    demo: "https://chat-app-mu-navy.vercel.app/login",
+  },
+  {
+    id: 3,
     image: IMG8,
     title: "Netflix-Clone",
     github: "https://github.com/dheeru2504/Netflix-clone",
-    demo: "https://netflix-clone-mu-seven.vercel.app/auth",
-  }
+    demo: "https://netflix-clone-e3vh3u72m-dheeru2504s-projects.vercel.app/auth",
+  },
+  {
+    id: 4,
+    image: IMG3,
+    title: "Keep Notes",
+    github: "https://github.com/Dheeru-k/Kepper-App",
+    demo: "https://keepnotees.netlify.app/",
+  },
+  {
+    id: 5,
+    image: IMG4,
+    title: "To Do List",
+    github: "https://github.com/Dheeru-k/todoList",
+    demo: "",
+  },
+  {
+    id: 6,
+    image: IMG5,
+    title: "Share Your Secrets",
+    github: "https://github.com/Dheeru-k/Secrets-1",
+    demo: "",
+  },
+
 ];
 
 const portfolio = () => {
